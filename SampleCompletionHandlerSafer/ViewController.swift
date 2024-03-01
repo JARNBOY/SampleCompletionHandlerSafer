a887ef97643398d6ef3faa229a4d202c497211ce
Papon <papon.s@lawyerplusgroup.com>
2024-03-01 19:12:25 +0700

    Initial Commit

---
+++ SampleCompletionHandlerSafer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCompletionHandlerSafer
+//
+//  Created by Papon Supamongkonchai on 1/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
